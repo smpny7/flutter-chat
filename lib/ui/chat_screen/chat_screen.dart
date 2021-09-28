3633fe3cc2c906ae6ec0bbb8a13b2039c30a6c06
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-09-28 15:09:18 +0900

    Fix: AppBar Shadow

--- lib/ui/chat_screen/chat_screen.dart
+++ lib/ui/chat_screen/chat_screen.dart
@@ -21,6 +21,7 @@ class _ChatScreenState extends State<ChatScreen> {
     final fetchMessageProvider = context.watch<FetchMessageProvider>();
     final userProvider = context.watch<UserProvider>();
     context.read<FetchMessageProvider>().fetchMessageList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -28,6 +29,7 @@ class _ChatScreenState extends State<ChatScreen> {
           style: TextStyle(color: Colors.grey.shade700),
         ),
         backgroundColor: Colors.white,
+        shadowColor: Colors.grey.shade50,
         actions: [
           IconButton(
             onPressed: () => UserBottomSheet().showBottomSheet(context),
@@ -56,8 +58,23 @@ class _ChatScreenState extends State<ChatScreen> {
                             Container(height: 16),
                           ],
                         )
-                      : const Text('メッセージなし')
-                  : const Text('読み込み中'),
+                      : Container()
+                  : Container(
+                      height: MediaQuery.of(context).size.height - 190,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.1),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
             ),
           ),
           const TextInput(),
